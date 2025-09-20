708cf8ee21701e778d6a2bf88180340917d08617
Saurav Kumar <csaurav2014@gmail.com>
2025-09-20 14:03:49 +0530

    Learn about Expression, Lateral, Operators and type of operators.

---
+++ javascript_expression&operators.js
@@ -0,0 +1,68 @@
+// EXPRESSION
+// An expression in javascript is any valid unit code that procedures a value
+
+ //--> let a = , 3+3 (it is a expression)
+
+// LATERAL
+// A literal is fixed value that you write directly in the code
+
+ //--> 30, "javascript", true (They are all lateral)
+
+// OPERATORS
+// Operators in javaScript are symbols that perform operations on operands.
+
+ //--> +, -, /, * (They are all operators)
+
+ /*
+ Types of Operators in JavaScript:
+
+ 1. Arithmetic Operators:
+
+    - Addition (+)
+    - Substraction (-)
+    - Multiplication (*)
+    - Division (/)
+    - Modulus (%)
+    - Increment (++)
+    - Decrement (--)
+
+
+2. Assignment Operators:
+
+    - Assignment (=)
+    - Addition aasignment (+=)
+    - Substraction assignment (-=)
+    - Multiplication assignment (*=)
+    - Division assignment (/=)
+
+
+3. Comparison Operaors:
+
+    - Equality (==)
+    - Strict equality (===)
+    - Inequality (!=)
+    - Strict inequality (!==)
+    - Greater than (>)
+    - Less than (<)
+    - Greater than or equal to (>=)
+    - Less than or equal to (<=)
+
+
+4. Logical Operators:
+
+    - Logical AND (&&)
+    - Logical OR (||)
+    - Logical NOT (!)
+
+5. Unary Operators:
+
+    - Increment (++)
+    - Decrement (--)
+    - typeOf operator
+    - Logical NOT (!)
+
+6. Ternary Operator:
+
+    - Conditional Operator (condition ? value1 : value2)
+    
+ */
