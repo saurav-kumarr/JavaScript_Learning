9e107db0da59bd3388d4580051a775d951903fe3
Saurav Kumar <csaurav2014@gmail.com>
2025-09-20 13:39:42 +0530

    Learninig of JS is which type of language. It is dynamic

---
+++ javascript_typed_lang.js
@@ -0,0 +1,21 @@
+// STATICALLY TYPED LANGUAGE Example:
+// Java varibale cannot change their type during the program's execution
+
+// int age = 30;(Define datatype of variable must)
+// age = "Java";
+
+
+// JAVASCRIPT IS DYNAMICALLY TYPED LANGUAGE
+// Variables can change their type during the program's execution
+
+let myVariable;
+console.log(typeof myVariable);
+
+myVariable = 1;
+console.log(typeof myVariable);
+
+myVariable = "Javascript";
+console.log(typeof myVariable);
+
+myVariable = true;
+console.log(typeof myVariable);
