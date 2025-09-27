48cf393b86ae91139f40d90f5f337db471c2089c
Saurav Kumar <csaurav2014@gmail.com>
2025-09-27 12:18:02 +0530

    Learn about For & While Loops in JS

---
+++ javascript_for_loop.js
@@ -0,0 +1,32 @@
+// For and While loops
+
+// For Loops
+for(let i = 0;i < 10;i++){
+    console.log(i);
+}
+
+
+for(let i = 10;i >= 0;i--){
+    console.log(i);
+}
+
+let numbers = [1,2,3,4,5,6,7,8];
+for(let i=0;i<numbers.length;i++){
+    console.log(numbers[i]);
+}
+
+for(let i = 0;i < 5;i++){
+    for(let j = 0;j<2;j++){
+    console.log(j);
+  }
+}
+
+// While Loop
+// Executes the statements as long as condition
+// evalutes to true
+
+let i=0;
+while (i<5) {
+    console.log(i);
+    i++;
+}
