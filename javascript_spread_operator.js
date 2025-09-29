d4f4485d7c57fd7b2870932f0c82e3c63f6ee50c
Saurav Kumar <csaurav2014@gmail.com>
2025-09-29 11:50:09 +0530

    Learn about spread operator and it applications in JS

---
+++ javascript_spread_operator.js
@@ -0,0 +1,29 @@
+// Spread Operator (...) : Used to expand elements of an iterable (like arrays or strings)
+// or properties of object into individual elements or properties.
+
+
+let originalArray = [1,2,3];
+console.log(originalArray);
+console.log(...originalArray);
+let copiedArray = [...originalArray];
+console.log(copiedArray);
+
+console.log(Math.max(...originalArray));
+
+let myString = "Hello";
+console.log(...myString);
+let stringArray = [...myString];
+console.log(stringArray);
+
+let mergedArray = [...originalArray, ...stringArray];
+console.log(mergedArray);
+
+function sum(a,b,c) {
+    return a+b+c;
+}
+console.log(sum(...originalArray));
+
+let object1 = {name : "Alice", age : 25};
+let object2 = {job: "Engineer", city: "New York"};
+let mergedObject = {...object1, ...object2};
+console.log(mergedObject)
