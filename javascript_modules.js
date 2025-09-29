c98ec872148166ca291b3bc2d2689821cc910378
Saurav Kumar <csaurav2014@gmail.com>
2025-09-29 12:50:26 +0530

    Learn about Modules in js and make module in deifferent file and then export it. After that import it in main file.

---
+++ javascript_modules.js
@@ -0,0 +1,9 @@
+// Modules help you break down large codebases into smaller, manageable parts.
+import { add, PI as pi} from './module_math.js'
+import sub from './module_math.js';
+
+console.log(add(5,3));
+//console.log(PI);
+console.log(pi);
+
+console.log(sub(1,2));
---
+++ module_math.js
@@ -0,0 +1,13 @@
+export const PI = 3.14;
+
+export function add(a,b) {
+    return a+b;
+}
+
+export default function sub (a,b) {
+    return a-b;
+}
+
+// Also use as follow...
+// export default sub;
+// export {add,PI};
